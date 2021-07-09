e9b5adcd40065dbe65b5cd05c4fe262aa4af0ed4
Juste Niyonteze <justirenee10@gmail.com>
2021-07-09 14:07:34 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+fetch('https://puzzle.mead.io/puzzle').then((response) => {
+	response.json().then((data) => {
+		console.log(data)
+	})
+})
+
+
+const form = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                return messageOne.textContent = data.error
+            }
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+	const url =
+		'http://api.weatherstack.com/current?access_key=9747b0c1511e9d01b726b1eb60d60fe9&query=' +
+		latitude +
+		',' +
+		longitude
+
+	request({ url, json: true }, (error, { body }) => {
+		if (error) {
+			callback('No network available', undefined)
+		} else if (body.error) {
+			callback('Please provide valid coordinates')
+		} else {
+			const data = body.current
+			callback(
+				undefined,
+					'It is currently ' +
+					data.temperature +
+					' degrees outside. It feels like ' +
+					data.feelslike +
+					' degrees outside.'
+			)
+		}
+	})
+}
+
+module.exports = forecast
